61f27f9af8984962b478f3a00c92b96feea0895d
z.terehov <zaharkuli67@gmail.com>
2023-01-13 23:30:56 +0300

    added base entiti

---
+++ YourMovies/YourMovies.Domain/Entities/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourMovies.Domain.Entities
+{
+    public abstract class Entity
+    {
+        public virtual Guid Id { get; protected set;}
+
+        public Entity(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
