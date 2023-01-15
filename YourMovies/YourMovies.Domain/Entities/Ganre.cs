e2ef31cef29c46bc14ef42a07a706fb59710b113
z.terehov <zaharkuli67@gmail.com>
2023-01-15 22:54:58 +0300

    added ganre

---
+++ YourMovies/YourMovies.Domain/Entities/Ganre.cs
@@ -0,0 +1,16 @@
+﻿
+namespace YourMovies.Domain.Entities
+{
+    public class Ganre : Entity
+    {
+        string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public Ganre(Guid id, string name, string description) : base(id)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
