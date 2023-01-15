6c2fab4ad8d9405aa8b24049081fa66964e80cc8
z.terehov <zaharkuli67@gmail.com>
2023-01-15 22:55:10 +0300

    added country

---
+++ YourMovies/YourMovies.Domain/Entities/Country.cs
@@ -0,0 +1,17 @@
+﻿
+namespace YourMovies.Domain.Entities
+{
+    public class Country : Entity
+    {
+        string ISOCode { get; set; }
+
+        string Name { get; set; }
+
+
+        public Country(Guid id, string iSOCode, string name) : base(id)
+        {
+            ISOCode = iSOCode;
+            Name = name;
+        }
+    }
+}
