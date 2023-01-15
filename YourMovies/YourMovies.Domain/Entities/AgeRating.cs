ce03375d9ffe4574681a21a7d10f0eaffa4159d7
z.terehov <zaharkuli67@gmail.com>
2023-01-15 22:55:29 +0300

    added ageRating

---
+++ YourMovies/YourMovies.Domain/Entities/AgeRating.cs
@@ -0,0 +1,19 @@
+﻿
+namespace YourMovies.Domain.Entities
+{
+    public class AgeRating : Entity
+    {
+        public string Name { get; private set; }
+
+        public string ViewAge { get; private set; }
+
+        public string Description { get; private set; }
+
+        public AgeRating(Guid id, string name, string viewAge, string desctiption) : base(id)
+        {
+            Name = name;
+            ViewAge = viewAge;
+            Description = desctiption;
+        }
+    }
+}
