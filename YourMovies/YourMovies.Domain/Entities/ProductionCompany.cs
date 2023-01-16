c70c95370df3db5d5cf02602fef5c30c24a37ad3
z.terehov <zaharkuli67@gmail.com>
2023-01-16 23:12:04 +0300

    added ProductionCompany

---
+++ YourMovies/YourMovies.Domain/Entities/ProductionCompany.cs
@@ -0,0 +1,12 @@
+﻿
+namespace YourMovies.Domain.Entities
+{
+    public class ProductionCompany : Entity
+    {
+        public string Name { get; set; }
+
+        public ProductionCompany(Guid id) : base(id)
+        {
+        }
+    }
+}
