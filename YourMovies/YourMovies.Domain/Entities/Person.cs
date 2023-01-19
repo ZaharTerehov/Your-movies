dd0229530295de2b976b19c6106de57e4a7066a8
z.terehov <zaharkuli67@gmail.com>
2023-01-19 21:34:47 +0300

    added class Person

---
+++ YourMovies/YourMovies.Domain/Entities/Person.cs
@@ -0,0 +1,14 @@
+﻿
+namespace YourMovies.Domain.Entities
+{
+    public class Person : Entity
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public Person(Guid id) : base(id)
+        {
+        }
+    }
+}
