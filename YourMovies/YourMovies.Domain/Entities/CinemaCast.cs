4a1d51b6fc4a82f243ba0ea5d4aadbabd75be62a
z.terehov <zaharkuli67@gmail.com>
2023-01-20 22:24:30 +0300

    added class CinemaCast

---
+++ YourMovies/YourMovies.Domain/Entities/CinemaCast.cs
@@ -0,0 +1,16 @@
+﻿
+namespace YourMovies.Domain.Entities
+{
+    public class CinemaCast : Entity
+    {
+        Person Person { get; set; }
+
+        string CharacterName { get; set; }
+
+        decimal CastOrder { get; set; }
+
+        public CinemaCast(Guid id) : base(id)
+        {
+        }
+    }
+}
