b0428614b95da4684acd3826537c35c5bd28a76f
z.terehov <zaharkuli67@gmail.com>
2023-01-20 22:24:02 +0300

    added class cinemeCrew

---
+++ YourMovies/YourMovies.Domain/Entities/CinemaCrew.cs
@@ -0,0 +1,16 @@
+﻿
+namespace YourMovies.Domain.Entities
+{
+    public class CinemaCrew : Entity
+    {
+        Person Person { get; set; }
+
+        Department Department { get; set; }
+
+        string Job { get; set; }
+
+        public CinemaCrew(Guid id) : base(id)
+        {
+        }
+    }
+}
