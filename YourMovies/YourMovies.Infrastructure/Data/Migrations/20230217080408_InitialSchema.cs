92322579102f95e8d4491584c0ca6507483e3998
z.terehov <zaharkuli67@gmail.com>
2023-02-17 22:35:34 +0300

    added migration

---
+++ YourMovies/YourMovies.Infrastructure/Data/Migrations/20230217080408_InitialSchema.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YourMovies.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AgeRatings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ViewAge = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AgeRatings", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ISOCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ganre",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ganre", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductionCompany",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductionCompany", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TypeCinema",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TypeCinema", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CinemaCast",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PersonId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CharacterName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CastOrder = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CinemaCast", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CinemaCast_Person_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Person",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CinemaCrew",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PersonId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DepartmentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Job = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CinemaCrew", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CinemaCrew_Department_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Department",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CinemaCrew_Person_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Person",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cinema",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AgeRatingId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TypeCinemaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    GanreId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CountryId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductionCompanyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CinemaCastId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CinemaCrewId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReleaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MainImage = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FilmBudget = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cinema", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cinema_AgeRatings_AgeRatingId",
+                        column: x => x.AgeRatingId,
+                        principalTable: "AgeRatings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Cinema_CinemaCast_CinemaCastId",
+                        column: x => x.CinemaCastId,
+                        principalTable: "CinemaCast",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Cinema_CinemaCrew_CinemaCrewId",
+                        column: x => x.CinemaCrewId,
+                        principalTable: "CinemaCrew",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Cinema_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Cinema_Ganre_GanreId",
+                        column: x => x.GanreId,
+                        principalTable: "Ganre",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Cinema_ProductionCompany_ProductionCompanyId",
+                        column: x => x.ProductionCompanyId,
+                        principalTable: "ProductionCompany",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Cinema_TypeCinema_TypeCinemaId",
+                        column: x => x.TypeCinemaId,
+                        principalTable: "TypeCinema",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cinema_AgeRatingId",
+                table: "Cinema",
+                column: "AgeRatingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cinema_CinemaCastId",
+                table: "Cinema",
+                column: "CinemaCastId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cinema_CinemaCrewId",
+                table: "Cinema",
+                column: "CinemaCrewId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cinema_CountryId",
+                table: "Cinema",
+                column: "CountryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cinema_GanreId",
+                table: "Cinema",
+                column: "GanreId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cinema_ProductionCompanyId",
+                table: "Cinema",
+                column: "ProductionCompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cinema_TypeCinemaId",
+                table: "Cinema",
+                column: "TypeCinemaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CinemaCast_PersonId",
+                table: "CinemaCast",
+                column: "PersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CinemaCrew_DepartmentId",
+                table: "CinemaCrew",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CinemaCrew_PersonId",
+                table: "CinemaCrew",
+                column: "PersonId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cinema");
+
+            migrationBuilder.DropTable(
+                name: "AgeRatings");
+
+            migrationBuilder.DropTable(
+                name: "CinemaCast");
+
+            migrationBuilder.DropTable(
+                name: "CinemaCrew");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+
+            migrationBuilder.DropTable(
+                name: "Ganre");
+
+            migrationBuilder.DropTable(
+                name: "ProductionCompany");
+
+            migrationBuilder.DropTable(
+                name: "TypeCinema");
+
+            migrationBuilder.DropTable(
+                name: "Department");
+
+            migrationBuilder.DropTable(
+                name: "Person");
+        }
+    }
+}
