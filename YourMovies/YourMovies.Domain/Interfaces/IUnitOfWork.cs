a4a6910958b9071efb9b54c6241b6cbf66866714
z.terehov <zaharkuli67@gmail.com>
2023-02-18 22:08:26 +0300

    added IUnitOfWork

---
+++ YourMovies/YourMovies.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿
+using YourMovies.Domain.Entities;
+
+namespace YourMovies.Domain.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IRepository<AgeRating> AgeRatings { get; }
+        IRepository<Cinema> Cinema { get; }
+        IRepository<CinemaCast> CinemaCasts { get; }
+        IRepository<CinemaCrew> CinemaCrews { get; }
+        IRepository<Country> Countrys { get; }
+        IRepository<Department> Departments { get; }
+        IRepository<Ganre> Ganres { get; }
+        IRepository<Person> Persons { get; }
+        IRepository<ProductionCompany> ProductionCompanys { get; }
+        IRepository<TypeCinema> TypeCinema { get; }
+    }
+}
