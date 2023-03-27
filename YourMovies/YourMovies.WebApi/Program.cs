28da5750c1dae5ba4b2cf4ac05af25cab5fd2699
z.terehov <zaharkuli67@gmail.com>
2023-03-27 10:06:41 +0300

    Added Program

---
+++ YourMovies/YourMovies.WebApi/Program.cs
@@ -0,0 +1,61 @@
+using YourMovies.WebApi.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var provider = builder.Services.BuildServiceProvider();
+var configuration = provider.GetRequiredService<IConfiguration>();
+
+builder.Services.AddCors(options =>
+{
+    var frontendUrl = configuration.GetValue<string>("FrontendUrl");
+    options.AddPolicy("CORSPolicy",
+        builder =>
+        {
+            builder
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .WithOrigins("http://localhost:3000");
+        });
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins(frontendUrl).AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
+YourMovies.Infrastructure.Dependencies.ConfigureServices(builder.Configuration, builder.Services);
+
+builder.Services.AddCoreServices();
+
+//AutoMapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(swaggerGenOptions =>
+{
+    swaggerGenOptions.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "YourMovies Project", Version= "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseSwagger();
+app.UseSwaggerUI(swaggerUIOptions =>
+{
+    swaggerUIOptions.DocumentTitle = "Web API YourMovies";
+    swaggerUIOptions.SwaggerEndpoint("/swagger/v1/swagger.json", "Web API serving.");
+});
+
+app.UseHttpsRedirection();
+
+app.UseCors();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
