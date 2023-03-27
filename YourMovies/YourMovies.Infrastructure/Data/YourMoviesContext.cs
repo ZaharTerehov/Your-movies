ffb18cbbec3c7ce37ac3eaa16ea970e1d43f20f9
z.terehov <zaharkuli67@gmail.com>
2023-03-27 09:59:15 +0300

    Added db context

---
+++ YourMovies/YourMovies.Infrastructure/Data/YourMoviesContext.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using YourMovies.Domain.Entities;
+
+namespace YourMovies.Infrastructure.Data
+{
+    public class YourMoviesContext : DbContext
+    {
+        public DbSet<AgeRating> AgeRatings { get; set; }
+        public DbSet<Cinema> Cinema { get; set; }
+        public DbSet<CinemaCast> CinemaCast { get; set; }
+        public DbSet<CinemaCrew> CinemaCrew { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Ganre> Ganre { get; set; }
+        public DbSet<Person> Person { get; set; }
+        public DbSet<ProductionCompany> ProductionCompany { get; set; }
+        public DbSet<TypeCinema> TypeCinema { get; set; }
+
+        public YourMoviesContext(DbContextOptions<YourMoviesContext> options) : base(options) { }
+    }
+}
