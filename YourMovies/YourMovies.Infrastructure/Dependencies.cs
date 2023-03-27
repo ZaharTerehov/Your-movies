7b2437acc8439d0383cc45aa2f5b9a1b383acc06
z.terehov <zaharkuli67@gmail.com>
2023-03-27 10:02:49 +0300

    Added dependencies

---
+++ YourMovies/YourMovies.Infrastructure/Dependencies.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using YourMovies.Infrastructure.Data;
+
+namespace YourMovies.Infrastructure
+{
+    public static class Dependencies
+    {
+        public static void ConfigureServices(IConfiguration configuration, IServiceCollection services)
+        {
+            services.AddDbContext<YourMoviesContext>(context =>
+            context.UseSqlServer(configuration.GetConnectionString("YourMoviesConnection")));
+        }
+    }
+}
