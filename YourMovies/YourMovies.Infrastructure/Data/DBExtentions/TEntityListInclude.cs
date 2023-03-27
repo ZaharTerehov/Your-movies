07c6153808514909341d971504c9a3f0999a9d20
z.terehov <zaharkuli67@gmail.com>
2023-03-27 09:58:04 +0300

    added TEntityListInclude

---
+++ YourMovies/YourMovies.Infrastructure/Data/DBExtentions/TEntityListInclude.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourMovies.Infrastructure.Data.DBExtentions
+{
+    public static class TEntityListInclude
+    {
+        public static IQueryable<TEntity> IncludeFields<TEntity>(this IQueryable<TEntity> entities, IList<Expression<Func<TEntity, object>>> includeOptions) where TEntity : class
+        {
+            if (includeOptions != null && includeOptions.Count > 0)
+            {
+                var firstOption = includeOptions.First();
+                var query = entities.Include(firstOption);
+                foreach (var item in includeOptions.Skip(1))
+                {
+                    query = query.Include(item);
+                }
+                return query;
+            }
+            return entities;
+        }
+    }
+}
