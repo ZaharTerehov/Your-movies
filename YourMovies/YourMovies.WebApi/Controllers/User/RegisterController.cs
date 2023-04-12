ef95df2cc62302771e1533ef20e2010e1af23a5f
z.terehov <zaharkuli67@gmail.com>
2023-04-12 21:00:18 +0300

    Added RegisterController

---
+++ YourMovies/YourMovies.WebApi/Controllers/User/RegisterController.cs
@@ -0,0 +1,71 @@
+﻿
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+using YourMovies.Application.Contracts.User;
+using YourMovies.Application.Interfaces;
+using YourMovies.Application.Interfaces.Account;
+using YourMovies.Application.Interfaces.Account.Token;
+
+namespace YourMovies.WebApi.Controllers.User
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        private readonly IGenerateAccessTokenService _generateAccessToken;
+        private readonly IGenerateConfirmEmailTokenService _generateConfirmEmailToken;
+        private readonly IEmailSenderService _emailSender;
+
+        public string LocationAccessToken { get; init; } = "YourMovies.Application.AT";
+        public string LocationRefreshToken { get; init; } = "YourMovies.Application.RT";
+
+        public RegisterController(IUnitOfWork unitOfWork, IGenerateAccessTokenService generateAccessToken, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _generateAccessToken = generateAccessToken;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] CreateUserRequest request)
+        {
+            //var captchaValidation = await СheckСaptchaTokenForValidity(model.ReCaptcha);
+            
+            //if (captchaValidation.StatusCode != StatusCode.OK)
+            //    return captchaValidation;
+            
+            var users = await _unitOfWork.Users.GetAllAsync();
+
+            if (users.Select(_ => _.Email == request.Email) == null)
+                return BadRequest("There is already a user with this login");
+            
+            var newUser = _mapper.Map<Domain.Entities.User>(request);
+            
+            await _unitOfWork.Users.CreateAsync(newUser);
+            
+            var token = await GenerateEmailConfirmationToken(newUser);
+            
+            newUser.EmailVerificationToken = token.token;
+            
+            await _unitOfWork.Users.UpdateAsync(newUser);
+            
+            await _emailSender.SendEmailAsync(newUser.Email, "Confirm your email", "Click on the link to confirm email - " +token.link);
+            
+            return Ok(newUser);
+        }
+
+        private async Task<(string link, string token)> GenerateEmailConfirmationToken(Domain.Entities.User user)
+        {
+            var confirmEmailToken = await _generateConfirmEmailToken.GenerateConfirmEmailToken(user);
+
+            var validEmailToken = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(confirmEmailToken.Token));
+
+            return ($"https://localhost:7227/Account/ConfirmEmail?userid={user.Id}&token={validEmailToken}", validEmailToken);
+        }
+    }
+}
