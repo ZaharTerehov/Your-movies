d6783c89a90dc67684153392d859923e924ec12b
z.terehov <zaharkuli67@gmail.com>
2023-04-12 08:21:46 +0300

    Added enum "Role"

---
+++ YourMovies/YourMovies.Domain/Enums/Role.cs
@@ -0,0 +1,13 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace YourMovies.Domain.Enums
+{
+    public enum Role
+    {
+        [Display(Name = "User")]
+        User = 0,
+        [Display(Name = "Admin")]
+        Admin = 1
+    }
+}
