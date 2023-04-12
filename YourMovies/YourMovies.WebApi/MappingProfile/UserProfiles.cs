8ae766777d6d967d6b70f7ed704a9b25538ba2b9
z.terehov <zaharkuli67@gmail.com>
2023-04-12 21:02:03 +0300

    Added mapping for user

---
+++ YourMovies/YourMovies.WebApi/MappingProfile/UserProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using YourMovies.Application.Contracts.User;
+using YourMovies.Domain.Entities;
+
+namespace YourMovies.WebApi.MappingProfile
+{
+    public class UserProfiles : Profile
+    {
+        public UserProfiles() 
+        {
+            CreateMap<User, CreateUserRequest>();
+            CreateMap<CreateUserRequest, User>();
+        }
+    }
+}
