11c2222637c75fcb7e874a40b6da9e3863fa5cec
z.terehov <zaharkuli67@gmail.com>
2023-04-12 21:01:35 +0300

    Added contracts "CreateUserRequest" and "BaseResponse"

---
+++ YourMovies/YourMovies.Application/Contracts/BaseResponse.cs
@@ -0,0 +1,12 @@
+﻿
+namespace YourMovies.Application.Contracts
+{
+    public sealed class BaseResponse<T> where T : class
+    {
+        public string? Description { get; set; }
+
+        //public StatusCode? StatusCode { get; set; }
+
+        public T? Data { get; set; }
+    }
+}
---
+++ YourMovies/YourMovies.Application/Contracts/User/CreateUserRequest.cs
@@ -0,0 +1,10 @@
+﻿
+namespace YourMovies.Application.Contracts.User
+{
+    public sealed class CreateUserRequest
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
