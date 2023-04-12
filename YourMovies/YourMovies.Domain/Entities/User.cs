af434d35607aa3b244ee00fa3534c9021277a06c
z.terehov <zaharkuli67@gmail.com>
2023-04-12 21:00:46 +0300

    Added User entity

---
+++ YourMovies/YourMovies.Domain/Entities/User.cs
@@ -0,0 +1,49 @@
+﻿
+using YourMovies.Domain.Enums;
+
+namespace YourMovies.Domain.Entities
+{
+    public sealed class User : Entity
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime RegistrationDate { get; set; } = DateTime.Now;
+
+        public Role Role { get; set; } = Role.User;
+
+        public string? RefreshToken { get; set; }
+
+        public DateTime RefreshTokenExpiryInMinutes { get; set; }
+
+        public bool EmailIsVerified { get; set; } = false;
+
+        public string EmailVerificationToken { get; set; } = string.Empty;
+
+        public void UpdateDetails(UserDetails details)
+        {
+            Email = details.Email;
+            Password = details.Password;
+        }
+
+        public void UpdateRefreshToken(string refreshToken, DateTime refreshTokenExpiryInMinutes)
+        {
+            RefreshToken = refreshToken;
+            RefreshTokenExpiryInMinutes = refreshTokenExpiryInMinutes;
+        }
+    }
+
+    public readonly record struct UserDetails
+    {
+        public string Email { get; }
+
+        public string Password { get; }
+
+        public UserDetails(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+    }
+}
