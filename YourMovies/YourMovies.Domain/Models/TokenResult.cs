79b0d5595c611ff19b4de4368be058912d39172c
z.terehov <zaharkuli67@gmail.com>
2023-04-13 22:37:58 +0300

    Added TokenResult

---
+++ YourMovies/YourMovies.Domain/Models/TokenResult.cs
@@ -0,0 +1,16 @@
+﻿
+namespace YourMovies.Domain.Models
+{
+    public sealed class TokenResult
+    {
+        public string Token { get; set; }
+
+        public DateTime Expires { get; set; }
+
+        public TokenResult(string token, DateTime expires)
+        {
+            Token = token;
+            Expires = expires;
+        }
+    }
+}
