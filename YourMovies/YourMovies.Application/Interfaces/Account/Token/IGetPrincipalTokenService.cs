9b544592fd069d11e2175ba31f57fa42a8c31eef
z.terehov <zaharkuli67@gmail.com>
2023-04-15 23:43:26 +0300

    Added token interfaces

---
+++ YourMovies/YourMovies.Application/Interfaces/Account/Token/IGetPrincipalTokenService.cs
@@ -0,0 +1,11 @@
+﻿
+using System.IdentityModel.Tokens.Jwt;
+using YourMovies.Domain.Models;
+
+namespace YourMovies.Application.Interfaces.Account.Token
+{
+    public interface IGetPrincipalTokenService
+    {
+        Task<JwtSecurityToken> GetPrincipalToken(TokenResult token);
+    }
+}
